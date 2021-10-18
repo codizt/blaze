8b44497b800426225d8e6fd993d9a3552c12008d
Dev <thecodizt@gmail.com>
2021-10-18 10:40:34 +0530

    ⚡ Boiler plate

---
+++ src/theme.ts
@@ -0,0 +1,32 @@
+import { createTheme } from "@mui/material/styles";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff3d00",
+    },
+    secondary: {
+      main: "#212121",
+    },
+    info: {
+      main: "#2196f3",
+    },
+    background: {
+      default: "#212121",
+    },
+    text: {
+      primary: "#ffffff",
+      secondary: "#ffffff",
+      disabled: "rgba(255,255,255,0.81)",
+    },
+    error: {
+      main: "#e53935",
+    },
+    success: {
+      main: "#ff3d00",
+    },
+  },
+});
+
+export default theme;
